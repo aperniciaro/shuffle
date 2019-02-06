517a492209e75e8601e4af900e364b48108a1d92
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-06 16:39:34 -0500

    simplified shuffle

--- public/main.js
+++ public/main.js
@@ -14,38 +14,47 @@ let cardValues = [
   'Ace'
 ]
 let cardSuits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-let unshuffledDeck = []
-let shuffledDeck = []
+let deck = []
 
 const reload = () => {
   buildDeck()
   shuffle()
+  document.querySelector('button').classList.add('empty-deck')
+  document.querySelector('button').classList.remove('draw-pile')
 }
 
 const buildDeck = () => {
   for (let cVal = 0; cVal < cardValues.length; cVal++) {
     for (let cSuit = 0; cSuit < cardSuits.length; cSuit++) {
-      unshuffledDeck.push(cardValues[cVal] + ' of ' + cardSuits[cSuit])
+      deck.push(cardValues[cVal] + ' of ' + cardSuits[cSuit])
     }
   }
-  // console.log(unshuffledDeck)
 }
 
 const shuffle = () => {
-  for (let n = unshuffledDeck.length - 1; n >= 0; n--) {
-    let randInt = Math.floor(Math.random() * n)
-    shuffledDeck.push(unshuffledDeck[randInt])
-    unshuffledDeck.splice(randInt, 1)
+  for (let i = deck.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+
+    let posI = deck[i]
+    let posJ = deck[j]
+
+    deck[i] = posJ
+    deck[j] = posI
   }
-  console.log(shuffledDeck)
+  console.log(deck)
 }
 
 const drawCard = () => {
-  const yourCard = document.createElement('li')
-  yourCard.textContent = shuffledDeck[0]
-  document.querySelector('.visible-hand').appendChild(yourCard)
-  shuffledDeck.splice(0, 1)
-  console.log(shuffledDeck)
+  if (deck.length > 0) {
+    const yourCard = document.createElement('li')
+    yourCard.textContent = deck[0]
+    document.querySelector('.visible-hand').appendChild(yourCard)
+    deck.splice(0, 1)
+  } else {
+    // document.querySelector('button').classList.remove('draw-pile')
+    // document.querySelector('button').classList.add('empty-deck')
+  }
+  console.log(deck)
 }
 
 document.addEventListener('DOMContentLoaded', reload)
