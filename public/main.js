cb9bac3bd3d951437a9152943a6f791b462187f6
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-06 14:52:35 -0500

    build deck function

--- public/main.js
+++ public/main.js
@@ -1,32 +1,59 @@
+let cardValues = [
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
+  'Ten',
+  'Jack',
+  'Queen',
+  'King',
+  'Ace'
+]
+let cardSuits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+let unshuffledDeck = []
+let shuffledDeck = []
+
 const reload = () => {
-  if () {
-  }
+  buildDeck()
 }
+
+const buildDeck = () => {
+  for (let cVal = 0; cVal < cardValues.length; cVal++) {
+    for (let cSuit = 0; cSuit < cardSuits.length; cSuit++) {
+      unshuffledDeck.push(cardValues[cVal] + ' of ' + cardSuits[cSuit])
+    }
+  }
+  console.log(unshuffledDeck)
+}
+
+const shuffle = () => {}
 
 // load page function
 
-  //   array of card values
-  //   array of suits
+//   array of card values
+//   array of suits
 
-  //   function with loop concatenating card values and suits and pushing to new array 
+//   function with loop concatenating card values and suits and pushing to new array
 
-  //   array of cards
+//   array of cards
 
-  //   function randomizing array of cards
+//   function randomizing array of cards
 
-    //   loop swapping items from array of cards with shuffled array
+//   loop swapping items from array of cards with shuffled array
 
-  //   shuffled array 
+//   shuffled array
 
 // event click
 
-  //   hand array
+//   hand array
 
-  //   remove top card from shuffled array 
-  //   add top card to hand array 
+//   remove top card from shuffled array
+//   add top card to hand array
 
-  //   put new hand array item as li in ul in DOM
-
-
+//   put new hand array item as li in ul in DOM
 
 document.addEventListener('DOMContentLoaded', reload)
