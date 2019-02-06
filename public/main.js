4b66b25bdc7710191c8ea7b39ae3be7a50c638a1
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-06 14:00:22 -0500

    pseudocode outline

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const reload = () => {
+  if () {
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// load page function
+
+  //   array of card values
+  //   array of suits
+
+  //   function with loop concatenating card values and suits and pushing to new array 
+
+  //   array of cards
+
+  //   function randomizing array of cards
+
+    //   loop swapping items from array of cards with shuffled array
+
+  //   shuffled array 
+
+// event click
+
+  //   hand array
+
+  //   remove top card from shuffled array 
+  //   add top card to hand array 
+
+  //   put new hand array item as li in ul in DOM
+
+
+
+document.addEventListener('DOMContentLoaded', reload)
